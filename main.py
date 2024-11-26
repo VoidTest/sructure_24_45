2f525e92d8540d45710ab2e2267f870df2945ab2
ReinisDelvers <reinis.delvers@gmail.com>
2024-11-26 10:56:53 +0200

    .

--- main.py
+++ main.py
@@ -7,7 +7,30 @@ app = Flask(__name__)
 def index():
     answer = requests.get("https://api.chucknorris.io/jokes/random")
     joke = answer.json()
-    return render_template("index.html", joke = joke["value"], image = joke["icon_url"])
+    answer = requests.get("https://api.chucknorris.io/jokes/categories")
+    categories = answer.json()
+    if request.method == "POST" :
+        categorie = request.form["cat"]
+        answer = requests.get(f"https://api.chucknorris.io/jokes/random?category={categorie}")
+        joke = answer.json()
+    return render_template("index.html", joke = joke["value"], image = joke["icon_url"], categories = categories)
+
+@app.route("/uni")
+def uni():
+    answer = requests.get("http://universities.hipolabs.com/search?country=latvia")
+    all = answer.json()
+    print(all[3]["web_pages"][0])
+    nosaukumi = []
+    for elements in all:
+        added = {
+            "nosaukums" : elements["name"],
+            "majaslapas" : elements["web_pages"]
+        }
+        nosaukumi.append(added)
+
+    return render_template("uni.html", uni=nosaukumi)
 
 if __name__ == '__main__':
     app.run(port = 5000)
+
+
